d1b8da02701861af7379749029c455cc1336ce3b
Mindiest <mindy15963@naver.com>
2023-04-17 19:43:24 +0900

    update

--- B. 기본 알고리즘/1. 선택 정렬.py
+++ B. 기본 알고리즘/1. 선택 정렬.py
@@ -1,8 +1,12 @@
 # 선택 정렬 (Selection Sort)
 # 데이터 배열에서 가장 작은 데이터를 선택하여 앞으로 보내는 정렬이다.
 
-import sorting as so
-
 data=list(map(int,input('배열 입력 : ').split()))
-result=so.selection(data)
-print('선택 정렬 결과 값 : ',result)
+for i in range(len(data) - 1):
+    min_idx = i
+    for j in range(i + 1, len(data)):
+        if data[j] < data[min_idx]:
+            min_idx = j
+    data[i], data[min_idx] = data[min_idx], data[i]
+    print(data)
+print('선택 정렬 결과 값 : ',data)
