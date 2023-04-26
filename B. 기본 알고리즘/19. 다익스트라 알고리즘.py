9b3c514cab5ce39b7ba7f18dbc6b2de6d7058c70
Mindiest <mindy15963@naver.com>
2023-04-26 19:20:17 +0900

    update

---
+++ B. 기본 알고리즘/19. 다익스트라 알고리즘.py
@@ -0,0 +1,17 @@
+# 다익스트라 알고리즘 (Dijkstra Algorithm)
+# 음의 가중치가 없는 그래프의 한 정점에서 모든 정점까지의 최단거리를 각각 구하는 알고리즘(최단 경로 문제)이다.
+
+import networkx as nx
+
+nodes=list(input('노드 값 입력 : ').split())
+G = nx.MultiDiGraph()
+G.add_nodes_from(nodes)
+ec=int(input('엣지의 개수 입력 : '))
+for i in range(ec):
+    d,a=input('엣지 값 입력 : ').split()
+    w=int(input('가중치 입력 : '))
+    G.add_edge(d,a,weight=w)
+so=input('출발지 입력 : ')
+ta=input('도착지 입력 : ')
+d_path=nx.dijkstra_path(G, so, ta)
+print('결과 값 : \n',d_path)
