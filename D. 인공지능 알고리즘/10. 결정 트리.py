144a2d65ea2030d0c445591c478f95b9612afc71
Mindiest <mindy15963@naver.com>
2023-06-07 19:43:21 +0900

    update

--- D. 인공지능 알고리즘/10. 결정 트리.py
+++ D. 인공지능 알고리즘/10. 결정 트리.py
@@ -4,29 +4,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as img
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import *
 from sklearn.datasets import make_blobs
-from sklearn.tree import export_graphviz
-from subprocess import check_call
 
 dc=int(input('데이터의 개수 입력 : '))
 x, y = make_blobs(n_samples=dc, centers=4, random_state=2,n_features=2,cluster_std=2)
 
-tree_model = DecisionTreeClassifier(max_depth=3)
-tree_model.fit(x,y)
+dt = DecisionTreeClassifier(max_depth=3)
+dt.fit(x,y)
 
-export_graphviz(
-    tree_model,
-    out_file = './tree_model.dot',
-    rounded=True,
-    filled=True
-)
-
-check_call(['dot','-Tpng','tree_model.dot','-o','OutputFile.png'])
-
-tm=img.imread('OutputFile.png')
-
-plt.rcParams['font.family']='Malgun Gothic'
-plt.title('결정 트리')
-plt.imshow(tm)
+plt.figure(figsize=(10,7))
+plot_tree(dt)
 plt.show()
