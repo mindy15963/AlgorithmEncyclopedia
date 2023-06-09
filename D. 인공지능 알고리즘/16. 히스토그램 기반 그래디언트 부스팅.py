521e6885b3eb5d32a6377adc751732728e865612
Mindiest <mindy15963@naver.com>
2023-06-09 18:32:19 +0900

    update

--- D. 인공지능 알고리즘/16. 히스토그램 기반 그래디언트 부스팅.py
+++ D. 인공지능 알고리즘/16. 히스토그램 기반 그래디언트 부스팅.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import HistGradientBoostingClassifier
 from sklearn.datasets import make_moons
 from sklearn.metrics import accuracy_score
+from sklearn.inspection import permutation_importance
 
 dc=int(input('데이터의 개수 입력 : '))
 x, y = make_moons(n_samples=dc, noise=0.25, random_state=3)
@@ -12,7 +13,6 @@ x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_sta
 
 hgb = HistGradientBoostingClassifier(random_state=42)
 hgb.fit(x_train,y_train)
-
 y_pred=hgb.predict(x_test)
 
 print(f"정확도 : {accuracy_score(y_test,y_pred)}")
