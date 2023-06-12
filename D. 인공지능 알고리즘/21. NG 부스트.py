7186895c51e38843a4763c319b615dbe6dbb0e84
Mindiest <mindy15963@naver.com>
2023-06-12 21:17:30 +0900

    update

--- D. 인공지능 알고리즘/21. NG 부스트.py
+++ D. 인공지능 알고리즘/21. NG 부스트.py
@@ -2,20 +2,17 @@
 # 확률론적 예측을 위한 자연 그래디언트 부스팅이다.
 
 from sklearn.model_selection import train_test_split
-from ngboost import NGBRegressor
+from ngboost import NGBClassifier
+from ngboost.distns import Bernoulli
 from sklearn.datasets import make_moons
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import roc_auc_score
 
 dc=int(input('데이터의 개수 입력 : '))
 x, y = make_moons(n_samples=dc, noise=0.25, random_state=3)
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
 
-ngb = NGBRegressor().fit(x_train,y_train)
-y_preds = ngb.predict(x_test)
-y_dists = ngb.pred_dist(x_test)
+ngb = NGBClassifier(Dist=Bernoulli)
+ngb.fit(x_train, y_train)
 
-test_MSE = mean_squared_error(y_preds, y_test)
-print('평균제곱오차 값 : ', test_MSE)
-
-test_NLL = -y_dists.logpdf(y_test).mean()
-print('음의 로그우도 값 : ', test_NLL)
+preds = ngb.pred_dist(x_test)
+print("ROC : ", roc_auc_score(y_test, preds.probs[1]))
