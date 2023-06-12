86162c681db6385e86fbd6e1b5c714652c2b6a73
Mindiest <mindy15963@naver.com>
2023-06-12 19:02:57 +0900

    update

--- D. 인공지능 알고리즘/16. XG 부스트.py
+++ D. 인공지능 알고리즘/16. XG 부스트.py
@@ -1,8 +1,8 @@
-# 히스토그램 기반 그래디언트 부스팅(Histogram-based Gradient Boosting)
-# 앙상블 학습 기법 중 하나로, 여러 개의 결정 트리를 조합하여 예측 모델을 구축하는 알고리즘이다.
+# XG 부스트(eXtream Gradient Boosting)
+# 최적화된 분산 그래디언트 부스팅 알고리즘을 구현하는 오픈소스 머신러닝 라이브러리이다.
 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import HistGradientBoostingClassifier
+from xgboost import XGBClassifier
 from sklearn.datasets import make_moons
 from sklearn.metrics import accuracy_score
 
@@ -10,8 +10,8 @@ dc=int(input('데이터의 개수 입력 : '))
 x, y = make_moons(n_samples=dc, noise=0.25, random_state=3)
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
-hgb = HistGradientBoostingClassifier(random_state=42)
-hgb.fit(x_train,y_train)
-y_pred=hgb.predict(x_test)
+xgb = XGBClassifier(tree_method='hist',random_state=42)
+xgb.fit(x_train,y_train)
+y_pred=xgb.predict(x_test)
 
 print(f"정확도 : {accuracy_score(y_test,y_pred)}")
