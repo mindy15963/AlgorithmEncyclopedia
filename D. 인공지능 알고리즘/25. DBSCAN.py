6c25db59d51faeee90f5fdfc6b4782ad0b494e8b
Mindiest <mindy15963@naver.com>
2023-06-14 20:11:31 +0900

    update

--- D. 인공지능 알고리즘/25. DBSCAN.py
+++ D. 인공지능 알고리즘/25. DBSCAN.py
@@ -1,5 +1,5 @@
 # DBSCAN (Density-Based Spatial Clustering of Applications with Noise)
-# 특정 데이터를 중심으로 밀도가 높은 곳에 포함된 데이터에는 클러스터를 할당하고 밀도가 낮으면 그 데이터를 노이즈로 취급하는 알고리즘이다.
+# 특정 데이터를 중심으로 밀도가 높은 곳에 포함된 데이터에는 군집를 할당하고 밀도가 낮으면 그 데이터를 노이즈로 취급하는 알고리즘이다.
 
 import numpy as np
 from sklearn.cluster import DBSCAN
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 ns=int(input('표본 데이터의 수 입력 : '))
-ct=int(input('생성할 클러스터의 수 입력 : '))
+ct=int(input('생성할 군집의 수 입력 : '))
 
 x,y=make_blobs(n_samples=ns,centers=ct,cluster_std=0.4,random_state=0)
 X = StandardScaler().fit_transform(x)
@@ -24,7 +24,7 @@ core_samples_mask[db.core_sample_indices_] = True
 
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise = list(labels).count(-1)
-print('클러스터 개수 : ', n_clusters_)
+print('군집 개수 : ', n_clusters_)
 print('노이즈 개수 : ', n_noise)
 print('동질성 : %0.3f' % metrics.homogeneity_score(y, labels))
 print("완전성 : %0.3f" % metrics.completeness_score(y, labels))
---
+++ D. 인공지능 알고리즘/26. 가우시안 혼합 모델.py
@@ -0,0 +1,26 @@
+# 가우시안 혼합 모델 (Gaussian Mixture Model, GMM)
+# 가우시안 분포가 여러 개 혼합된 군집화 알고리즘이다.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+
+ns=int(input('표본 데이터의 수 입력 : '))
+ct=int(input('생성할 군집의 수 입력 : '))
+
+x,y=make_blobs(n_samples=ns,centers=ct,cluster_std=0.4,random_state=42)
+
+gmm = GaussianMixture(n_components=ct)
+gmm.fit(x)
+
+labels = gmm.predict(x)
+
+plt.rcParams['font.family']='Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] = False
+plt.scatter(x[:, 0], x[:, 1], c=labels, cmap='viridis')
+plt.xlabel('x 값')
+plt.ylabel('y 값')
+plt.title('가우시안 혼합 모델 군집화')
+plt.show()
