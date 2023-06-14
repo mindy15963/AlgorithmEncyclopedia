d9961ee704a1128f0881b6e59387a4a477bed33b
Mindiest <mindy15963@naver.com>
2023-06-14 20:45:36 +0900

    update

--- D. 인공지능 알고리즘/29. OPTICS.py
+++ D. 인공지능 알고리즘/29. OPTICS.py
@@ -1,9 +1,9 @@
-# 유사도 전파 군집화 (Affinity Propagation Clustering)
-# 데이터 지점 간의 "메시지 전달" 개념을 기반으로 하는 군집화 알고리즘이다.
+# OPTICS (Ordering Points To Identify the Clustering Structure)
+# 밀도 기반의 군집을 탐색하기 위한 알고리즘이다.
 
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import OPTICS
 from sklearn.datasets import make_blobs
 import warnings
 
@@ -11,18 +11,19 @@ warnings.filterwarnings('ignore')
 
 ns=int(input('표본 데이터의 수 입력 : '))
 ct=int(input('생성할 군집의 수 입력 : '))
+ms=int(input('최소 샘플 개수 입력 : '))
 
-x,y=make_blobs(n_samples=ns,centers=ct,cluster_std=0.6,random_state=0)
+x,y=make_blobs(n_samples=ns,centers=ct,cluster_std=1.5,random_state=40)
 
-ap = AffinityPropagation(random_state=5)
-ap.fit(x)
+op = OPTICS(min_samples=ms, xi=0.02, min_cluster_size=0.1)
+op.fit(x)
 
-labels = ap.labels_
+labels = op.labels_
 
 plt.rcParams['font.family']='Malgun Gothic'
 plt.rcParams['axes.unicode_minus'] = False
 plt.scatter(x[:, 0], x[:, 1], c=labels)
 plt.xlabel('x 값')
 plt.ylabel('y 값')
-plt.title('유사도 전파 군집화')
+plt.title('OPTICS')
 plt.show()
