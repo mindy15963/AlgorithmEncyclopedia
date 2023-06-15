721a43fd0e47d73f62d6b43a20ef19884da23f8e
Mindiest <mindy15963@naver.com>
2023-06-15 20:15:54 +0900

    update

--- D. 인공지능 알고리즘/32. 교차 검증 1.py
+++ D. 인공지능 알고리즘/32. 교차 검증 1.py
@@ -2,18 +2,19 @@
 # 데이터를 여러 번 반복해서 나누고 여러 모델을 학습하여 성능을 평가하는 방법이다.
 
 from sklearn.model_selection import cross_val_score
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_moons
 from sklearn.linear_model import LogisticRegression
 
 ns=int(input('표본 데이터의 수 입력 : '))
 c=int(input('겹수 입력 : '))
 
-x,y=make_blobs(n_samples=ns)
+x,y=make_moons(n_samples=ns,random_state=3)
 
 lr = LogisticRegression()
 
 basic_scores= cross_val_score(lr,x,y)
 cv_scores= cross_val_score(lr,x,y,cv=c)
 
-print('기본 교차 검증 정확도 : ', basic_scores)
-print(f'{c}겹 교차 검증 정확도 : ', cv_scores)
+print('기본 교차 검증 점수 : ', basic_scores)
+print(f'{c}겹 교차 검증 점수 : ', cv_scores)
+print("교차 검증 평균 점수 : {:.2f}".format(basic_scores.mean()))
