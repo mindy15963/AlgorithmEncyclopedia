f2d8729b1f2011bef7673289eae83347a1eb2957
Mindiest <mindy15963@naver.com>
2023-06-16 17:29:34 +0900

    update

---
+++ D. 인공지능 알고리즘/35. LOOCV 교차 검증.py
@@ -0,0 +1,19 @@
+# LOOCV 교차 검증 (LOOCV Cross Validation)
+# 폴드 하나에 샘플 하나만 들어 있는 k-겹 교차 검증으로 각 반복에서 하나의 데이터 포인트를 선택해 테스트 세트로 사용한다.
+
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_moons
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import LeaveOneOut
+
+ns=int(input('표본 데이터의 수 입력 : '))
+
+x,y=make_moons(n_samples=ns,random_state=3)
+
+lr = LogisticRegression()
+loo = LeaveOneOut()
+
+scores = cross_val_score(lr,x,y,cv=loo)
+
+print("교차 검증 분할 횟수 : ", len(scores))
+print("평균 정확도 : {:.2f}".format(scores.mean()))
