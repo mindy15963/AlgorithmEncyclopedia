cf86bad643e71c6707852d6e491b30a2725ff5fc
Mindiest <mindy15963@naver.com>
2023-06-16 19:52:20 +0900

    update

--- D. 인공지능 알고리즘/39. 반복 교차 검증 2.py
+++ D. 인공지능 알고리즘/39. 반복 교차 검증 2.py
@@ -1,7 +1,6 @@
 # 반복 교차 검증 (Repeated Cross Validation)
 # 데이터셋의 크기가 크지 않을 경우 안정된 검증 점수를 얻기 위해 교차 검증을 반복하여 여러 번 수행하는 교차 검증 방식이다.
 
-import matplotlib.pyplot as plt
 from sklearn.datasets import make_moons
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import *
