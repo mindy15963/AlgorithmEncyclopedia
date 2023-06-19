eada0f3b409dae63ec602225639c9b9b20fc58a7
Mindiest <mindy15963@naver.com>
2023-06-19 19:43:26 +0900

    update

--- D. 인공지능 알고리즘/40. 계층별 K-겹 교차 검증.py
+++ D. 인공지능 알고리즘/40. 계층별 K-겹 교차 검증.py
@@ -1,10 +1,10 @@
-# K-겹 교차 검증 (K-fold Cross Validation)
-# 데이터를 k개로 분할한 뒤, k-1개를 학습용 데이터 세트로, 1개를 평가용 데이터 세트로 사용하는데, 이 방법을 k번 반복하여 k개의 성능 지표를 얻어내는 방법이다.
+# 계층별 K-겹 교차 검증 (Stratified K-fold Cross Validation)
+# 데이터 클래스가 불균형한 경우 클래스 별 분포를 고려해서 데이터 폴드 세트는 만드는 방법이다.
 
 from sklearn.model_selection import cross_val_score
 from sklearn.datasets import make_moons
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LinearRegression
 
 ns=int(input('표본 데이터의 수 입력 : '))
@@ -13,7 +13,7 @@ c=int(input('겹수 입력 : '))
 x,y=make_moons(n_samples=ns,random_state=3)
 
 lr = LogisticRegression()
-kfold = KFold(n_splits = c)
+kfold = StratifiedKFold(n_splits = c)
 reg = LinearRegression()
 
 print("1. 분류 모델 교차 검증 점수 (분할기 사용) : \n", cross_val_score(lr, x, y, cv=kfold))
