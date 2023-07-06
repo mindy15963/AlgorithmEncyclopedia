158fbe87211530b706b7474c2a7ea43c45b2f928
Mindiest <mindy15963@naver.com>
2023-07-06 19:46:03 +0900

    update

---
+++ E. 암호 알고리즘/04. 힐 암호.py
@@ -0,0 +1,11 @@
+# 힐 암호 (Hill Cipher)
+# 선형 대수학을 기반으로 하는 다중 문자 대체 암호이다.
+
+from kryptor.hill_cipher import HillCipher
+plain=input('평문 입력 : ')
+obj = HillCipher()
+key=[list(map(int, input(f'{_+1}차원 키 입력 : ').split())) for _ in range(2)]
+en=obj.encrypt(plain,key)
+print('암호문 : ',en)
+de=obj.decrypt(en,key)
+print('복호문 : ',de)
