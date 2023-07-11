cb0bebc54152683c55d6d27c121275f4a963f086
Mindiest <mindy15963@naver.com>
2023-07-11 17:58:31 +0900

    update

---
+++ E. 암호 알고리즘/08. DES.py
@@ -0,0 +1,18 @@
+# DES (Data Encryption Standard)
+# 64비트 평문을 64비트 암호문으로 암호화하는 대칭키 암호 알고리즘이다.
+
+from Crypto.Cipher import DES
+def pad(text):
+    while len(text) % 8 != 0:
+        text += ' '
+    return text
+
+key = input('키 입력 : ').encode()
+text1 = input('평문 입력 : ')
+
+des = DES.new(key, DES.MODE_ECB)
+padded_text = pad(text1)
+encrypted_text = des.encrypt(padded_text.encode())
+
+print('암호문 : ',encrypted_text)
+print('복호문 : ',des.decrypt(encrypted_text))
